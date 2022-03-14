e0968e307955de01a7d61e2ccf40e896b1f83015
chua <chua3940@gmail.com>
2022-03-14 17:21:26 +0800

    fixed google ml kit and firebase clash

--- lib/face_detector.dart
+++ lib/face_detector.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'dart:math';
 import 'dart:ui' as ui;
-import 'package:google_ml_kit/google_ml_kit.dart';
+//import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'face_painter.dart'; import 'package:image_picker/image_picker.dart';
